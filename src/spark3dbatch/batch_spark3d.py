76a9cbe4d7957adff43fcb40eac4a22fa81c24cd
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2025-06-04 09:55:07 +0200

    updated to do list

--- src/spark3dbatch/batch_spark3d.py
+++ src/spark3dbatch/batch_spark3d.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
-"""Provide an example main script."""
+"""Provide an example main script.
+
+.. todo::
+    Fix paths with importlib
+
+"""
 from pathlib import Path
 
 import numpy as np
